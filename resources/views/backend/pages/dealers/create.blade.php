826bd77f3d36a3acaf04d607ba6b16fc40e8acca
Bini Amin <titubiniamin@gmail.com>
2024-11-06 21:00:40 +0600

    a

--- resources/views/backend/pages/dealers/create.blade.php
+++ resources/views/backend/pages/dealers/create.blade.php
@@ -6,7 +6,7 @@
 
 @section('admin-content')
 
-    <!-- page title area start -->
+    <!-- Page title area start -->
     <div class="page-title-area">
         <div class="row align-items-center">
             <div class="col-sm-6">
@@ -23,10 +23,10 @@
             </div>
         </div>
     </div>
-    <!-- page title area end -->
+    <!-- Page title area end -->
 
     <div class="main-content-inner">
-        <form action="{{ route('admin.dealers.store') }}" method="POST"> <!-- Form starts here -->
+        <form action="{{ route('admin.dealers.store') }}" method="POST">
             @csrf
             <div class="row">
                 <!-- Left column for form inputs -->
@@ -47,30 +47,32 @@
                                             </ul>
                                         </div>
                                     @endif
+
                                     <!-- Display success message -->
                                     @if (session('success'))
                                         <div id="flash-message" class="alert alert-success">
                                             {{ session('success') }}
                                         </div>
                                     @endif
+
+                                    <!-- Flash message fade-out script -->
                                     <script>
-                                        // Automatically hide the flash message after 5 seconds
                                         setTimeout(function() {
                                             const flashMessage = document.getElementById('flash-message');
                                             if (flashMessage) {
-                                                flashMessage.style.transition = 'opacity 0.5s ease'; // Fade-out transition
-                                                flashMessage.style.opacity = '0'; // Start fading
-
-                                                setTimeout(() => flashMessage.remove(), 500); // Remove from DOM after fade-out
+                                                flashMessage.style.transition = 'opacity 0.5s ease';
+                                                flashMessage.style.opacity = '0';
+                                                setTimeout(() => flashMessage.remove(), 500);
                                             }
-                                        }, 5000); // 5-second delay
+                                        }, 5000);
                                     </script>
 
-                                    <!-- Form Fields Start -->
+                                    <!-- Form fields -->
                                     <div class="form-group">
                                         <label for="name">Name</label>
                                         <input type="text" class="form-control" value="{{ old('name') }}" name="name" required>
                                     </div>
+
                                     <div class="form-group">
                                         <label for="owner_name">Owner Name</label>
                                         <input type="text" class="form-control" value="{{ old('owner_name') }}" name="owner_name" required>
@@ -78,7 +80,7 @@
 
                                     <div class="form-group">
                                         <label for="zone">Zone</label>
-                                        <input type="text" class="form-control" value="{{ old('zone')  }}" name="zone">
+                                        <input type="text" class="form-control" value="{{ old('zone') }}" name="zone">
                                     </div>
 
                                     <div class="form-group">
@@ -88,7 +90,7 @@
 
                                     <div class="form-group">
                                         <label for="email">Email</label>
-                                        <input type="email" class="form-control" value="email" name="email">
+                                        <input type="email" class="form-control" value="{{ old('email') }}" name="email">
                                     </div>
 
                                     <div class="form-group">
@@ -113,7 +115,7 @@
                                         <input type="text" name="district" id="district" hidden>
                                         <input type="text" class="form-control bksearch" name="location" id="location" />
                                         <div class="bklist"></div>
-                                        <div id="loading" style="display: none;">Loading...</div> <!-- Loading indicator -->
+                                        <div id="loading" style="display: none;">Loading...</div>
                                     </div>
 
                                     <div class="form-group">
@@ -121,8 +123,6 @@
                                     </div>
 
                                     <button type="submit" class="btn btn-primary">Save Dealer</button>
-                                    <!-- Form Fields End -->
-
                                 </div>
                             </div>
                         </div>
@@ -130,99 +130,77 @@
                 </div>
 
                 <!-- Right column for additional content -->
-                <div class="col-lg-3" style="height: 70vh;">
-                    <div class="card mt-5 mb-3" style="height: 400px;background-color: white">
+                <div class="col-lg-3">
+                    <div class="card mt-5 mb-3" style="height: 400px; background-color: white;">
                         <div class="p-4">
-                            <div class="form-group mb-4">
-                                <div class="form-group">
-                                    <label for="name">Average Sales</label>
-                                    <input type="text" class="form-control" value="{{ old('average_sales') }}" name="average_sales">
-                                </div>
-                                <div class="form-group">
-                                    <label for="name">Market Size</label>
-                                    <input type="text" class="form-control" value="{{ old('market_size') }}" name="market_size">
-                                </div>
-                                <div class="form-group">
-                                    <label for="market-share">Market Share</label>
-                                    <input type="text" class="form-control" value="{{ old('market_share') }}" name="market_share">
-                                </div>
-                                <div class="form-group">
-                                    <label for="name">Competition Brand</label>
-                                    <input type="text" class="form-control" value="{{ old('competition_brand') }}" name="competition_brand">
-                                </div>
+                            <div class="form-group">
+                                <label for="average_sales">Average Sales</label>
+                                <input type="text" class="form-control" value="{{ old('average_sales') }}" name="average_sales">
                             </div>
-                            <!-- Additional content goes here -->
+
+                            <div class="form-group">
+                                <label for="market_size">Market Size</label>
+                                <input type="text" class="form-control" value="{{ old('market_size') }}" name="market_size">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="market_share">Market Share</label>
+                                <input type="text" class="form-control" value="{{ old('market_share') }}" name="market_share">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="competition_brand">Competition Brand</label>
+                                <input type="text" class="form-control" value="{{ old('competition_brand') }}" name="competition_brand">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </form> <!-- Form ends here -->
+        </form>
     </div>
 
-    <!-- Your existing script and styles here -->
+    <!-- Scripts for map and autocomplete -->
+    <script>
+        bkoigl.accessToken = "{{ env('BARIKOI_API_KEY') }}";
 
-    <!-- Your existing script and styles here -->
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                console.log(latitude)
+                console.log(longitude)
+                initializeMap(latitude, longitude);
+                fetchLocationName(latitude, longitude);
+            },
+            (error) => {
+                console.error("Error fetching location:", error);
+            },
+            { enableHighAccuracy: true } // Request high-accuracy location
+        );
 
-
-<script>
-        bkoigl.accessToken = "{{ env('BARIKOI_API_KEY') }}"; // required
-
-        function getCurrentLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    const latitude = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-                    console.log('Latitude:', latitude);
-                    console.log('Longitude:', longitude);
-
-                    // Initialize the map with user's current coordinates
-                    initializeMap(latitude, longitude);
-
-                    // Fetch and set the address name using reverse geocoding
-                    fetchLocationName(latitude, longitude);
-
-                }, (error) => {
-                    console.error("Error fetching location:", error);
-                });
-            } else {
-                console.error("Geolocation is not supported by this browser.");
-            }
-        }
 
         function initializeMap(latitude, longitude) {
-            // Initialize map with the user's current coordinates
             const map = new bkoigl.Map({
                 container: "map",
-                center: [longitude, latitude], // center map at current location
+                center: [longitude, latitude],
                 zoom: 15,
             });
 
-            map.addControl(new bkoigl.FullscreenControl());
-            map.addControl(new bkoigl.NavigationControl());
-            map.addControl(new bkoigl.ScaleControl());
-
-            // Add a draggable marker at the user's location
             let marker = new bkoigl.Marker({ draggable: true })
                 .setLngLat([longitude, latitude])
                 .addTo(map);
 
-            // Event listener for marker drag to update location field
             marker.on('dragend', () => {
                 const lngLat = marker.getLngLat();
-                console.log('Marker dragged to:', lngLat);
-
-                // Update the address field and hidden fields with new coordinates
                 fetchLocationName(lngLat.lat, lngLat.lng);
             });
         }
 
         function fetchLocationName(latitude, longitude) {
-            // Fetch the address from the backend (reverse geocode)
             fetch(`/api/proxy/reverse-geocode?longitude=${longitude}&latitude=${latitude}`)
                 .then(response => response.json())
                 .then(data => {
                     if (data.place && data.place.address) {
-                        // Populate the location and hidden fields with fetched data
                         document.getElementById("location").value = data.place.address;
                         document.getElementById("longitude").value = longitude;
                         document.getElementById("latitude").value = latitude;
@@ -234,104 +212,65 @@
                 });
         }
 
-        // Load map and set address on page load
-        document.addEventListener("DOMContentLoaded", function () {
-            getCurrentLocation();
-        });
-
-
-
         document.getElementById("location").addEventListener("input", function () {
             let query = this.value;
             let loadingIndicator = document.getElementById("loading");
 
             if (query.length > 2) {
-                console.log('Searching for:', query);
-                loadingIndicator.style.display = "block"; // Show loading indicator
+                loadingIndicator.style.display = "block";
                 fetch(`/api/proxy/autocomplete?q=${query}`)
                     .then(response => response.json())
                     .then(data => {
-                        loadingIndicator.style.display = "none"; // Hide loading indicator
-                        if (data.places) {
-                            let suggestions = data.places;
-                            console.log('suggestion', suggestions);
+                        loadingIndicator.style.display = "none";
+                        let suggestionList = document.querySelector('.bklist');
+                        suggestionList.innerHTML = '';
 
-                            let suggestionList = document.querySelector('.bklist');
-                            suggestionList.innerHTML = ''; // Clear previous suggestions
-
-                            suggestions.forEach(place => {
-                                let suggestionItem = document.createElement('div');
-                                suggestionItem.textContent = place.address;
-                                suggestionItem.className = 'suggestion-item';
-                                suggestionItem.onclick = function () {
-                                    marker.setLngLat([place.longitude, place.latitude]);
-                                    map.flyTo({ center: [place.longitude, place.latitude], zoom: 15 });
-                                    suggestionList.innerHTML = '';
-                                    document.getElementById("location").value = place.address;
-                                    document.getElementById("longitude").value = place.longitude;
-                                    document.getElementById("latitude").value = place.latitude;
-                                    document.getElementById("district").value = place.district;
-                                };
-                                suggestionList.appendChild(suggestionItem);
-                            });
-                        }
+                        data.places.forEach(place => {
+                            let suggestionItem = document.createElement('div');
+                            suggestionItem.textContent = place.address;
+                            suggestionItem.className = 'suggestion-item';
+                            suggestionItem.onclick = function () {
+                                marker.setLngLat([place.longitude, place.latitude]);
+                                map.flyTo({ center: [place.longitude, place.latitude], zoom: 15 });
+                                suggestionList.innerHTML = '';
+                                document.getElementById("location").value = place.address;
+                                document.getElementById("longitude").value = place.longitude;
+                                document.getElementById("latitude").value = place.latitude;
+                                document.getElementById("district").value = place.district;
+                            };
+                            suggestionList.appendChild(suggestionItem);
+                        });
                     })
                     .catch(error => {
-                        loadingIndicator.style.display = "none"; // Hide loading indicator on error
-                        console.error('Error fetching data:', error);
+                        loadingIndicator.style.display = "none";
+                        console.error("Error fetching data:", error);
                     });
             } else {
-                document.querySelector('.bklist').innerHTML = ''; // Clear suggestions if query is too short
-                loadingIndicator.style.display = "none"; // Hide loading indicator if no query
+                document.querySelector('.bklist').innerHTML = '';
+                loadingIndicator.style.display = "none";
             }
         });
 
-        // Initialize the marker
-        let marker = new bkoigl.Marker({ draggable: true })
-            .setLngLat([90.3938010872331, 23.821600277500405])
-            .addTo(map);
-
-        // Event listener for marker drag end
-        marker.on('dragend', function() {
-            const lngLat = marker.getLngLat();
-            const longitude = lngLat.lng;
-            const latitude = lngLat.lat;
-
-            fetch(`/api/proxy/reverse-geocode?longitude=${longitude}&latitude=${latitude}`)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.place && data.place.address) {
-                        const locationInput = document.getElementById("location");
-                        const longitudeInput = document.getElementById("longitude");
-                        const latitudeInput = document.getElementById("latitude");
-                        const districtInput = document.getElementById("district");
-                        locationInput.value = data.place.address;
-                        districtInput.value = data.place.district;
-                        longitudeInput.value = longitude;
-                        latitudeInput.value = latitude;
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching address:', error);
-                });
-        });
+        document.addEventListener("DOMContentLoaded", getCurrentLocation);
     </script>
 
+    <!-- Styling for autocomplete suggestions -->
     <style>
-    .suggestion-item {
-        padding: 5px;
-        cursor: pointer;
-    }
-    .suggestion-item:hover {
-        background-color: #f0f0f0; /* Highlight on hover */
-    }
-    #loading {
-        display: none; /* Initially hidden */
-        font-size: 14px;
-        color: #888;
-        padding: 10px 0;
-    }
-
-
+        .bklist {
+            position: absolute;
+            z-index: 1000;
+            background-color: white;
+            border: 1px solid #ddd;
+            width: 100%;
+            display: none;
+        }
+        .bklist .suggestion-item {
+            padding: 10px;
+            cursor: pointer;
+        }
+        .bklist .suggestion-item:hover {
+            background-color: #f0f0f0;
+        }
     </style>
+
 @endsection
